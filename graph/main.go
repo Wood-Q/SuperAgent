a76a42886c047e10fbfab04453d9195ee9958b27
WOODQ <woodq41@gmail.com>
2025-05-13 11:55:42 +0800

    接入向量库成功，chain编排熟悉

--- graph/main.go
+++ graph/main.go
@@ -17,13 +17,13 @@ func main() {
 	initialize.InitChatModel()
 	ctx := context.Background()
 	chain := compose.NewChain[map[string]any, *schema.Message]().
-		AppendChatTemplate(prompt.FromMessages(schema.FString, schema.UserMessage("你叫什么名字？"))).
+		AppendChatTemplate(prompt.FromMessages(schema.FString, schema.SystemMessage("你的档案是{role},你要记住你自己的档案"), schema.UserMessage("你叫什么名字？"))).
 		AppendChatModel(global.ChatModel)
 	runnable, err := chain.Compile(ctx)
 	if err != nil {
 		fmt.Println("compile error: ", err)
 	}
-	result, err := runnable.Invoke(ctx, map[string]any{"query": "Beijing"})
+	result, err := runnable.Invoke(ctx, map[string]any{"role": "初音未来"})
 	if err != nil {
 		fmt.Println("invoke error: ", err)
 	}
--- initialize/indexer.go
+++ initialize/indexer.go
@@ -5,6 +5,7 @@ import (
 	"context"
 
 	"github.com/cloudwego/eino-ext/components/indexer/milvus"
+	"github.com/milvus-io/milvus-sdk-go/v2/entity"
 	"go.uber.org/zap"
 )
 
@@ -21,40 +22,40 @@ func InitIndexer() {
 		zap.S().Warn("Collection 'agent' 不存在，请先创建")
 	}
 
-	// fields := []*entity.Field{
-	// 	{
-	// 		Name:     "id",
-	// 		DataType: entity.FieldTypeVarChar,
-	// 		TypeParams: map[string]string{
-	// 			"max_length": "255",
-	// 		},
-	// 		PrimaryKey: true,
-	// 	},
-	// 	{
-	// 		Name:     "vector", // 确保字段名匹配
-	// 		DataType: entity.FieldTypeFloatVector,
-	// 		TypeParams: map[string]string{
-	// 			"dim": "2048",
-	// 		},
-	// 	},
-	// 	{
-	// 		Name:     "content",
-	// 		DataType: entity.FieldTypeVarChar,
-	// 		TypeParams: map[string]string{
-	// 			"max_length": "8102",
-	// 		},
-	// 	},
-	// 	{
-	// 		Name:     "meta_data",
-	// 		DataType: entity.FieldTypeJSON,
-	// 	},
-	// }
+	fields := []*entity.Field{
+		{
+			Name:     "id",
+			DataType: entity.FieldTypeVarChar,
+			TypeParams: map[string]string{
+				"max_length": "255",
+			},
+			PrimaryKey: true,
+		},
+		{
+			Name:     "vector", // 确保字段名匹配
+			DataType: entity.FieldTypeBinaryVector,
+			TypeParams: map[string]string{
+				"dim": "81920",
+			},
+		},
+		{
+			Name:     "content",
+			DataType: entity.FieldTypeVarChar,
+			TypeParams: map[string]string{
+				"max_length": "8192",
+			},
+		},
+		{
+			Name:     "metadata",
+			DataType: entity.FieldTypeJSON,
+		},
+	}
 
 	indexer, err := milvus.NewIndexer(context.Background(), &milvus.IndexerConfig{
-		Client:    client,
-		Embedding: global.Embedder,
-		// Collection: "agent",
-		// Fields:     fields,
+		Client:     client,
+		Embedding:  global.Embedder,
+		Collection: "try",
+		Fields:     fields,
 	})
 	if err != nil {
 		zap.S().Panic("Failed to create indexer: %v", zap.String("error", err.Error()))
